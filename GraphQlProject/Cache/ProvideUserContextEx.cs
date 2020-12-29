2b3bca03b7d2ae0de899d2c4dbda8cc41679ed25
ladni <ladnik@hotmail.com>
2020-12-29 12:43:11 +0200

    ProvideUserContextEx changed

--- GraphQlProject/Cache/ProvideUserContextEx.cs
+++ GraphQlProject/Cache/ProvideUserContextEx.cs
@@ -5,18 +5,11 @@ namespace GraphQlProject
 {
     public static class ProvideUserContextEx 
     {
-        public const string strCache = "cache";
-        public const string strCacheName = "cache-name";
-
-        public static Dictionary<string, object> GetCacheDictionary(this IProvideUserContext context) => 
-            context.UserContext.TryGetValue(strCache, out object obj)
-                ? obj as Dictionary<string, object>
-                : null;
+        private static IDictionary<string, object> GetCache(this IProvideUserContext context) => context.UserContext;
 
         public static object GetCache(this IProvideUserContext context, string key)
         {
-            var dct = ProvideUserContextEx.GetCacheDictionary(context);
-            dct.TryGetValue(key, out object cache);
+            ProvideUserContextEx.GetCache(context).TryGetValue(key, out object cache);
             return cache;
         }
 
@@ -25,11 +18,7 @@ namespace GraphQlProject
             if (cache == null)
                 return;
             
-            var dct = ProvideUserContextEx.GetCacheDictionary(context);
-            if (dct == null)
-                context.UserContext[strCache] = new Dictionary<string, object>();
-
-            ProvideUserContextEx.GetCacheDictionary(context)[key] = cache;
+            ProvideUserContextEx.GetCache(context)[key] = cache;
         }
     }
 }
