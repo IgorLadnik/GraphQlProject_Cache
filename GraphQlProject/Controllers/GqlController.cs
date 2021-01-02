f6b612596fcdec1918290ffe2b2ab05272055b39
ladni <ladnik@hotmail.com>
2021-01-02 07:18:41 +0200

    Controllers added

---
+++ GraphQlProject/Controllers/GqlController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GraphQL;
+using Newtonsoft.Json.Linq;
+using GraphQL.NewtonsoftJson;
+using GraphQL.Types;
+
+namespace GraphQlProject.Controllers
+{
+    [Route("gql")]
+    public class GqlController : Controller
+    {
+        private readonly IDocumentExecuter _documentExecuter;
+        private readonly ISchema _schema;
+        public GqlController(ISchema schema, IDocumentExecuter documentExecuter)
+        {
+            _schema = schema;
+            _documentExecuter = documentExecuter;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody] GraphqlQuery query)
+        {
+            if (query == null) 
+                throw new ArgumentNullException(nameof(query));
+            
+            var inputs = query.Variables.ToInputs();
+            var executionOptions = new ExecutionOptions
+            {
+                Schema = _schema,
+                Query = query.Query,
+                Inputs = inputs
+            };
+
+            var result = await _documentExecuter.ExecuteAsync(executionOptions);
+
+            if (result.Errors?.Count > 0)
+                return BadRequest(result);
+
+            return Ok(result.Data);
+        }
+    }
+
+    public class GraphqlQuery
+    {
+        public string OperationName { get; set; }
+        public string NamedQuery { get; set; }
+        public string Query { get; set; }
+        public JObject Variables { get; set; }
+    }
+}
