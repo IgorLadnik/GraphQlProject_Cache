bc9e377a46770f6d6b0e0326b7bad3e54ea59dee
ladni <ladnik@hotmail.com>
2021-01-10 09:15:31 +0200

    User fetch moved to AuthController

--- GraphQlProject/Controllers/AuthController.cs
+++ GraphQlProject/Controllers/AuthController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using GraphQlProject.Services;
+using GraphQlHelperLib;
+using GraphQlProject.Data;
 
 namespace GraphQlProject.Controllers
 {
@@ -10,10 +13,12 @@ namespace GraphQlProject.Controllers
     [ApiController]
     public class AuthController : Controller
     {
+        private DbProvider<GraphQLDbContext> _dbProvider;
         private AuthService _authService;
 
-        public AuthController(AuthService authService)
+        public AuthController(DbProvider<GraphQLDbContext> dbProvider, AuthService authService)
         {
+            _dbProvider = dbProvider;
             _authService = authService;
         }
 
@@ -26,7 +31,10 @@ namespace GraphQlProject.Controllers
         {
             try
             {
-                return Ok(await _authService.Login(userName, password));
+                var user = await _dbProvider.FetchAsync(dbContext => dbContext.Users
+                            .Where(u => u.UserName == userName && u.Password == password)
+                            .FirstOrDefault());
+                return Ok(await _authService.Login(user.UserName, user.Type));
             }
             //catch (UserNotFoundException e)
             //{
@@ -34,7 +42,7 @@ namespace GraphQlProject.Controllers
             //}
             catch (Exception e)
             {
-                return StatusCode(500);
+                return StatusCode(500, e.Message);
             }
         }
     }
--- GraphQlProject/Services/AuthService.cs
+++ GraphQlProject/Services/AuthService.cs
@@ -1,30 +1,21 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
-using GraphQlProject.Data;
-using GraphQlHelperLib;
+using GraphQlProject.Models;
 using JwtHelperLib;
-
 
 namespace GraphQlProject.Services
 {
     public class AuthService
     {
-        private DbProvider<GraphQLDbContext> _dbProvider;
         private JwtOptions _jwtOptions;
 
-        public AuthService(DbProvider<GraphQLDbContext> dbProvider, IConfiguration configuration)
+        public AuthService(IConfiguration configuration)
         {
-            _dbProvider = dbProvider;
             _jwtOptions = new JwtOptions(configuration);
         }
 
-    public async Task<string> Login(string userName, string password)
-        {
-            var user = await _dbProvider.FetchAsync(dbContext => dbContext.Users
-                            .Where(u => u.UserName == userName && u.Password == password)
-                            .FirstOrDefault());
-            return user != null ? JwtAuth.GenerateJwt($"{user.Id}", $"{user.Type}", _jwtOptions) : null;
-        }
+        public async Task<string> Login(string userName, UserAuthType userAuthType) =>
+              JwtAuth.GenerateJwt(userName, $"{userAuthType}", _jwtOptions);
     }
 }
