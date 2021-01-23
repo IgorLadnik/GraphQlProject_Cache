2483a5df5bee6f177bccc78b4a147572e2a82f74
ladni <ladnik@hotmail.com>
2021-01-23 15:55:32 +0200

    ConsoleClient with login and query

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var id = 2;
+            var query = @"   
+            {
+                personByIdQuery {
+                    personById(id: /*id*/) {
+                    surname
+                    relations {
+                        p2 {
+                        surname
+                        }
+                        kind
+                    }
+                    affiliations {
+                        organization {
+                        name
+                        }
+                        role {
+                        name
+                        }
+                    }
+                    }
+                }
+            }".Replace("/*id*/", $"{id}");
+
+            Task.Run(async () => 
+            {
+                var loginUri = "https://localhost:5011/login/fromCode";
+                var gqlUri = "https://localhost:5001/gql";
+                var userName = "Super";
+                var password = "SuperPassword";
+                var token = await Login(loginUri, userName, password);
+                var result = await QueryGraphQL(gqlUri, token, query);
+                Console.WriteLine(result);
+            });
+
+            Console.WriteLine("Press any key to quite...");
+            Console.ReadKey();
+        }
+
+        private static async Task<string> Login(string uri, string userName, string password)
+        {
+            HttpClient httpClient = new();
+            StringContent stringContent = new(JsonConvert.SerializeObject($"?UserName={userName}&Password={password}"), Encoding.UTF8, "application/json");
+            var res = await httpClient.PostAsync(uri, stringContent);
+            return res.IsSuccessStatusCode
+                ? await res.Content.ReadAsStringAsync()
+                : $"Error occurred... Status code:{res.StatusCode}";
+        }
+
+        private static async Task<string> QueryGraphQL(string uri, string token, string query)
+        {
+            HttpClient httpClient = new();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var postData = new { Query = query };
+            StringContent stringContent = new(JsonConvert.SerializeObject(postData), Encoding.UTF8, "application/json");
+            var res = await httpClient.PostAsync(uri, stringContent);
+            return res.IsSuccessStatusCode
+                ? await res.Content.ReadAsStringAsync()
+                : $"Error occurred... Status code:{res.StatusCode}";
+        }
+    }
+}
--- LoginService/Controllers/LoginController.cs
+++ LoginService/Controllers/LoginController.cs
@@ -1,9 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using RepoInterfaceLib;
 using JwtLoginLib;
 using JwtLoginLib.Data;
+using System.Net.Http;
+using System.Text.Json;
 
 namespace LoginService.Controllers
 {
@@ -26,6 +29,38 @@ namespace LoginService.Controllers
         {
             var result = await _authenticator.LoginAsync(userName, password);
             return _authenticator.IsOK ? Ok(result) : StatusCode(500, result);
+        }
+        
+        [HttpPost]
+        [Route("fromcode")]
+        //[ProducesResponseType(typeof(string), 200)]
+        //[ProducesResponseType(401)]
+        [AllowAnonymous]
+        public async Task<IActionResult> LoginAsync(JsonElement arg)
+        {
+            var str = arg.GetString();
+            if (string.IsNullOrEmpty(str))
+                return null;
+
+            var ss = str.Split(new string[] { "?", "=", "&" }, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+            if (ss?.Length == 4) 
+            {
+                string userName, password;
+                if (ss[0].ToLowerInvariant() == "username" && ss[2].ToLowerInvariant() == "password")
+                {
+                    userName = ss[1];
+                    password = ss[3];
+                }
+                else 
+                {
+                    userName = ss[3];
+                    password = ss[1];
+                }
+
+                return await LoginAsync(userName, password);
+            }
+
+            return null;
         }
     }
 }
