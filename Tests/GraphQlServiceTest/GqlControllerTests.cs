115f58ea8716409f4f0a3ba27ed9ff847e58017a
ladni <ladnik@hotmail.com>
2021-02-02 22:26:59 +0200

    Tests enhanced - 2

--- Tests/GraphQlServiceTest/GqlControllerTests.cs
+++ Tests/GraphQlServiceTest/GqlControllerTests.cs
@@ -1,39 +1,44 @@
-﻿using System.Net.Http;
-using System.Threading.Tasks;
-using GraphQlService;
-using HttpClientLib;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json.Linq;
+using HttpClientLib;
 
 namespace GraphQlServiceTest
 {
     [TestClass]
-    public class GqlControllerTests //: IntegrationTest
+    public class GqlControllerTests
     {
         const string query =
-            @"query PersonById {
-                  personByIdQuery {
-                    personById(id: -1) {
-                      surname
-                      relations {
-                        p2 {
-                          surname
-                        }
-                        kind
-                      }
-                      affiliations {
-                        organization {
-                          name
-                        }
-                        role {
-                          name
-                        }
+            @"query Persons {
+              personQuery {
+                persons {
+                  id
+                  givenName
+                  surname
+                  email
+                  affiliations {
+                    organization {
+                      name
+                      parent {
+                        name
                       }
                     }
+                    role {
+                      name
+                    }
+                  }
+                  relations {
+                    p2 {
+                      givenName
+                      surname
+                    }
+                    kind
+                    notes
                   }
                 }
-            ";
+              }
+            }";
 
         readonly string mutation =
             @"mutation PersonMutation {
@@ -48,85 +53,69 @@ namespace GraphQlServiceTest
                         address: '21, Torn Street'
 
                         affiliations: [{
-                        since: 2000
-                        organizationId: -4
-                        roleId: -1
+                            since: 2000
+                            organizationId: -4
+                            roleId: -1
                         }]
       		
                         relations: [{
-                        since: 2017
-                        kind: 'friend'
-                        notes: '*!'
-                        p2Id: -1
+                            since: 2017
+                            kind: 'friend'
+                            notes: '*!'
+                            p2Id: -1
                         }]					   
   		            }
                     ]
-                ) {
-                status
-                message
+                ) 
+                {
+                    status
+                    message
                 }
-                }
+              }
             }".Replace("'", "\"");
 
-        //var loginUri = "https://localhost:5011/login/fromCode";
-        //var gqlUri = "https://localhost:5001/gql";
-        //var userName = "Super";
-        //var password = "SuperPassword";
+        const string loginUri = "https://localhost:7000/login";
+        const string gqlUri = "https://localhost:7000/gql";
 
-        //HttpClientWrapper client = new();
-
-        //Task.Run(async () =>
-        //    {
-        //        string result = null;
-        //        if (await client.Login(loginUri, userName, password) != null)
-        //            result = await client.PostAsync(gqlUri, query);
-        //Console.WriteLine(client.IsOK? result : client.ErrorMessage);
-        //    });
-
-        const string gqlUri = "https://localhost:7001/gql";
+        const string userName = "Super";
+        const string password = "SuperPassword";
 
         const string token =
             "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IlN1cGVyIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiU3VwZXJVc2VyIiwiaXNzIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NTAwMS8iLCJhdWQiOiJodHRwczovL2xvY2FsaG9zdDo1MDAxLyJ9.TfTE49H0xyA6ZAAVBsu3TwmCwbnOwuGzlF2k2B5yHTQ";
 
-        private HttpClientWrapper _client;
+        private HttpClientWrapper _clientLogin;
+        private HttpClientWrapper _clientGql;
 
         public GqlControllerTests()
         {
-            _client = new(new WebApplicationFactory<Startup>().CreateClient(), token);
+            _clientLogin = new(new WebApplicationFactory<LoginService.Startup>().CreateClient());
+            _clientGql = new(new WebApplicationFactory<GraphQlService.Startup>().CreateClient(), token);
+        }
+
+        [TestMethod]
+        public async Task TestLogin()
+        {
+            var strToken = await _clientLogin.LoginAsync(loginUri, userName, password);
+            Assert.IsTrue(strToken == token);
         }
 
         [TestMethod]
         public async Task TestQuery()
         {
-            var content = await _client.PostAsync(gqlUri, query);
+            var content = await _clientGql.PostAsync(gqlUri, query);
 
-            //var jObject = JObject.Parse(content);
-            //var t = jObject["tradersQuery"]["traders"][0];
-            //Assert.IsNotNull(t);
-            //Assert.IsTrue((int)t["id"] == -1);
-            //Assert.IsTrue((string)t["email"] == "mcohen@trader.com");
-            //t = t["cryptocurrencies"];
-            //Assert.IsTrue((int)t[2]["id"] == 3);
-
-            //t = jObject["tradersQuery"]["traders"][1];
-            //Assert.IsNotNull(t);
-            //Assert.IsTrue((int)t["id"] == -2);
-            //Assert.IsTrue((string)t["email"] == "vpupkin@trader.com");
-            //t = t["cryptocurrencies"];
-            //Assert.IsTrue((int)t[0]["id"] == 1);
+            var jObject = JObject.Parse(content);
+            var t = jObject["personQuery"]["persons"][0];
+            Assert.IsTrue((string)t["email"] == "feretti@ub.ac.zz");
         }
 
         [TestMethod]
         public async Task TestMutation()
         {
-            var result = await _client.PostAsync(gqlUri, mutation);
+            var result = await _clientGql.PostAsync(gqlUri, mutation);
 
-            //var jObject = JObject.Parse(result);
-            //var jResult = jObject["tradersMutation"]["createTraders"];
-            //Assert.IsTrue((string)jResult["status"] == "Success");
-            //Assert.IsTrue((string)jResult["message"] == string.Empty);
-
-            // In addition for complete test, newly inserted / updated data should by queried and checked here.
+            var jObject = JObject.Parse(result);
+            Assert.IsTrue((string)jObject["personMutation"]["createPersons"]["status"] == "Success");
         }
     }
 }
